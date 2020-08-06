a86ae86bafb55d970ef3cab06d9ab8b9ec3ee7a5
Rappo <rapposelli.giacomo@gmail.com>
2020-08-06 02:31:11 +0200

    improved header

--- src/app.js
+++ src/app.js
@@ -5,26 +5,75 @@ import Gallery from "./gallery";
 import Store from "./store";
 
 export default function App() {
+    const [classHome, setClassHome] = useState("head-links");
+    const [classTour, setClassTour] = useState("head-links");
+    const [classGallery, setClassGallery] = useState("head-links");
+    const [classStore, setClassStore] = useState("head-links");
+
+    const updateHomeClass = () => {
+        setClassHome("head-links greyed");
+        setClassTour("head-links");
+        setClassGallery("head-links");
+        setClassStore("head-links");
+    };
+
+    const updateTourClass = () => {
+        setClassHome("head-links");
+        setClassTour("head-links greyed");
+        setClassGallery("head-links");
+        setClassStore("head-links");
+    };
+
+    const updateGalleryClass = () => {
+        setClassHome("head-links");
+        setClassTour("head-links");
+        setClassGallery("head-links greyed");
+        setClassStore("head-links");
+    };
+
+    const updateStoreClass = () => {
+        setClassHome("head-links");
+        setClassTour("head-links");
+        setClassGallery("head-links");
+        setClassStore("head-links greyed");
+    };
+
     return (
         <div className="container">
             <header>
                 <p>
-                    <a href="#home" className="head-links">
+                    <a
+                        href="#home"
+                        className={classHome}
+                        onClick={updateHomeClass}
+                    >
                         HOME
                     </a>
                 </p>
                 <p>
-                    <a href="#tour" className="head-links">
+                    <a
+                        href="#tour"
+                        className={classTour}
+                        onClick={updateTourClass}
+                    >
                         TOUR
                     </a>
                 </p>
                 <p>
-                    <a href="#gallery" className="head-links">
+                    <a
+                        href="#gallery"
+                        className={classGallery}
+                        onClick={updateGalleryClass}
+                    >
                         GALLERY
                     </a>
                 </p>
                 <p>
-                    <a href="#store" className="head-links">
+                    <a
+                        href="#store"
+                        className={classStore}
+                        onClick={updateStoreClass}
+                    >
                         STORE
                     </a>
                 </p>
@@ -38,31 +87,34 @@ export default function App() {
                     © 2020 Speedköbra - All rights reserved
                 </p>
                 <div className="logos">
-                    <a
-                        href="https://www.instagram.com/speedkobra/"
-                        target="_blank"
-                    >
-                        <img src="/insta.png" alt="" className="social-logo" />
+                    <a href="https://www.instagram.com/speedkobra/">
+                        <img
+                            src="/insta.png"
+                            alt=""
+                            className="social-logo"
+                            target="_blank"
+                        />
                     </a>
-                    <a
-                        href="https://www.facebook.com/speedkobra666"
-                        target="_blank"
-                    >
-                        <img src="/fb.png" alt="" className="social-logo" />
+                    <a href="https://www.facebook.com/speedkobra666">
+                        <img
+                            src="/fb.png"
+                            alt=""
+                            className="social-logo"
+                            target="_blank"
+                        />
                     </a>
-                    <a
-                        href="https://www.youtube.com/channel/UC57goUK4nTrwOGdhchCw8TA"
-                        target="_blank"
-                    >
-                        <img src="/you.png" alt="" className="social-logo" />
+                    <a href="https://www.youtube.com/channel/UC57goUK4nTrwOGdhchCw8TA">
+                        <img
+                            src="/you.png"
+                            alt=""
+                            className="social-logo"
+                            target="_blank"
+                        />
                     </a>
-                    <a
-                        href="https://open.spotify.com/artist/6bwLhL6tsar6mpLQTvnxr9"
-                        target="_blank"
-                    >
+                    <a href="https://open.spotify.com/artist/6bwLhL6tsar6mpLQTvnxr9">
                         <img
                             src="/spotify.png"
-                            alt=""
+                            target="_blank"
                             className="social-logo bc"
                         />
                     </a>
